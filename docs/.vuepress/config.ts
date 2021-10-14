4079279e4b121e15cbb4bbd28736decba2b22196
shizuku <2112165916@qq.com>
2021-10-14 19:54:40 +0800

    docs(dpln): create docs

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,29 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: "en-US",
+  title: "dpln",
+  description: "A tiny library for Deep Learning",
+  base: "/dpln/",
+  themeConfig: {},
+  bundler: "@vuepress/bundler-vite",
+  markdown: {
+    anchor: {},
+    extractHeaders: {
+      level: [2, 3],
+    },
+    importCode: {
+      handleImportPath: (str) => str,
+    },
+    links: {
+      internalTag: "RouterLink",
+      externalAttrs: { target: "_blank", rel: "noopener noreferrer" },
+      externalIcon: true,
+    },
+    toc: {
+      pattern: /^\[\[toc\]\]$/i,
+      level: [2, 3],
+    },
+  },
+});
