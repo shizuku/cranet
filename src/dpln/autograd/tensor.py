d59ae90d1f6192d25b97fe03d64406f90f7aa58d
shizuku <2112165916@qq.com>
2021-10-14 17:13:20 +0800

    feat(dpln.autograd): impl some functions
    impl: matmul, slice, transpose, permute

--- src/dpln/autograd/tensor.py
+++ src/dpln/autograd/tensor.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import (
     List,
+    Tuple,
     NamedTuple,
     Callable,
     Optional,
@@ -24,6 +25,16 @@ def ensure_array(arrayable: Arrayable) -> np.ndarray:
         return arrayable
     else:
         return np.array(arrayable)
+
+
+Tensorable = Union['Tensor', float, np.ndarray]
+
+
+def ensure_tensor(tensorable: Tensorable) -> Tensor:
+    if isinstance(tensorable, Tensor):
+        return tensorable
+    else:
+        return Tensor(tensorable)
 
 
 class Tensor:
@@ -55,8 +66,74 @@ class Tensor:
             backward_grad = dependency.grad_fn(grad.data)
             dependency.tensor.backward(Tensor(backward_grad))
 
+    def T(self):
+        pass
+
     def sum(self) -> Tensor:
-        return sum(self)
+        return _sum(self)
+
+    def transpose(self, dim1: int, dim2: int) -> Tensor:
+        return _transpose(self, dim1, dim2)
+
+    def permute(self, axes: Union[List, Tuple]) -> Tensor:
+        return _permute(self, axes)
+
+    def __add__(self, other) -> Tensor:
+        """gets called if I do t + other"""
+        return _add(self, ensure_tensor(other))
+
+    def __radd__(self, other) -> Tensor:
+        """gets called if I do other + t"""
+        return _add(ensure_tensor(other), self)
+
+    def __iadd__(self, other) -> Tensor:
+        """when we do t += other"""
+        self.data = self.data + ensure_tensor(other).data
+        return self
+
+    def __sub__(self, other) -> Tensor:
+        return _sub(self, ensure_tensor(other))
+
+    def __rsub__(self, other) -> Tensor:
+        return _sub(ensure_tensor(other), self)
+
+    def __isub__(self, other) -> Tensor:
+        """when we do t -= other"""
+        self.data = self.data - ensure_tensor(other).data
+        return self
+
+    def __neg__(self) -> Tensor:
+        return _neg(self)
+
+    def __mul__(self, other) -> Tensor:
+        return _mul(self, ensure_tensor(other))
+
+    def __rmul__(self, other) -> Tensor:
+        return _mul(ensure_tensor(other), self)
+
+    def __imul__(self, other) -> Tensor:
+        """when we do t *= other"""
+        self.data = self.data * ensure_tensor(other).data
+        return self
+
+    def __truediv__(self, other) -> Tensor:
+        return _truediv(self, ensure_tensor(other))
+
+    def __rtruediv__(self, other) -> Tensor:
+        return _truediv(ensure_tensor(other), self)
+
+    def __itruediv__(self, other) -> Tensor:
+        self.data = self.data / ensure_tensor(other).data
+        return self
+
+    def __matmul__(self, other) -> Tensor:
+        return _matmul(self, other)
+
+    def __getitem__(self, idxs):
+        return _slice(self, idxs)
+
+    def numpy(self) -> np.ndarray:
+        return self.data
 
     def __repr__(self) -> str:
         return f"Tensor({self.data}, shape={self.shape}, requires_grad={self.requires_grad}, dependencies={self.dependencies})"
@@ -67,7 +144,25 @@ class Tensor:
         return (self.data == other.data).all()
 
 
-def sum(t: Tensor) -> Tensor:
+def zeros(shape, dtype=None, order='C', requires_grad=False) -> Tensor:
+    return Tensor(np.zeros(shape=shape, dtype=dtype, order=order), requires_grad=requires_grad)
+
+
+def zeros_like(a, dtype=None, order='K', subok=True, shape=None, requires_grad=False) -> Tensor:
+    return Tensor(np.zeros_like(a=a, dtype=dtype, order=order, subok=subok, shape=shape), requires_grad=requires_grad)
+
+
+def ones(shape, dtype=None, order='C', *, like=None, requires_grad=False) -> Tensor:
+    return Tensor(np.ones(shape=shape, dtype=dtype, order=order, like=like), requires_grad=requires_grad)
+
+
+def ones_like(a: Tensor, dtype=None, requires_grad=False) -> Tensor:
+    if dtype is None:
+        dtype = a.data.dtype
+    return Tensor(np.ones_like(a=a, dtype=dtype, shape=a.shape), requires_grad=requires_grad)
+
+
+def _sum(t: Tensor) -> Tensor:
     """
     Takes a tensor and return the sum of its components
     """
@@ -84,23 +179,23 @@ def sum(t: Tensor) -> Tensor:
     return Tensor(data, requires_grad, dependencies)
 
 
-def zeros(shape, dtype=None, order='C', requires_grad=False) -> Tensor:
-    return Tensor(np.zeros(shape=shape, dtype=dtype, order=order), requires_grad=requires_grad)
+def _slice(t: Tensor, idxs) -> Tensor:
+    data = t.data[idxs]
+    requires_grad = t.requires_grad
+    dependencies: List[Dependency] = []
+
+    if requires_grad:
+        def grad_fn(grad: np.ndarray) -> np.ndarray:
+            bigger_grad = np.zeros_like(data)
+            bigger_grad[idxs] = grad
+            return bigger_grad
+
+        dependencies.append(Dependency(t, grad_fn))
+
+    return Tensor(data, requires_grad, dependencies)
 
 
-def zeros_like(a, dtype=None, order='K', subok=True, shape=None, requires_grad=False) -> Tensor:
-    return Tensor(np.zeros_like(a=a, dtype=dtype, order=order, subok=subok, shape=shape), requires_grad=requires_grad)
-
-
-def ones(shape, dtype=None, order='C', *, like=None, requires_grad=False) -> Tensor:
-    return Tensor(np.ones(shape=shape, dtype=dtype, order=order, like=like), requires_grad=requires_grad)
-
-
-def ones_like(a, dtype=None, order='K', subok=True, shape=None, requires_grad=False) -> Tensor:
-    return Tensor(np.ones_like(a=a, dtype=dtype, order=order, subok=subok, shape=shape), requires_grad=requires_grad)
-
-
-def add(t1: Tensor, t2: Tensor) -> Tensor:
+def _add(t1: Tensor, t2: Tensor) -> Tensor:
     data = np.add(t1.data, t2.data)
     requires_grad = t1.requires_grad or t2.requires_grad
     dependencies: List[Dependency] = []
@@ -132,7 +227,7 @@ def add(t1: Tensor, t2: Tensor) -> Tensor:
     return Tensor(data, requires_grad, dependencies)
 
 
-def sub(t1: Tensor, t2: Tensor) -> Tensor:
+def _sub(t1: Tensor, t2: Tensor) -> Tensor:
     data = t1.data - t2.data
     requires_grad = t1.requires_grad or t2.requires_grad
     dependencies: List[Dependency] = []
@@ -164,7 +259,7 @@ def sub(t1: Tensor, t2: Tensor) -> Tensor:
     return Tensor(data, requires_grad, dependencies)
 
 
-def neg(t: Tensor) -> Tensor:
+def _neg(t: Tensor) -> Tensor:
     data = np.negative(t.data)
     requires_grad = t.requires_grad
     dependencies: List[Dependency] = []
@@ -175,7 +270,7 @@ def neg(t: Tensor) -> Tensor:
     return Tensor(data, requires_grad, dependencies)
 
 
-def mul(t1: Tensor, t2: Tensor) -> Tensor:
+def _mul(t1: Tensor, t2: Tensor) -> Tensor:
     data = t1.data * t2.data
     requires_grad = t1.requires_grad or t2.requires_grad
     dependencies: List[Dependency] = []
@@ -209,7 +304,7 @@ def mul(t1: Tensor, t2: Tensor) -> Tensor:
     return Tensor(data, requires_grad, dependencies)
 
 
-def div(t1: Tensor, t2: Tensor) -> Tensor:
+def _truediv(t1: Tensor, t2: Tensor) -> Tensor:
     data = np.divide(t1.data, t2.data)
     requires_grad = t1.requires_grad or t2.requires_grad
     dependencies: List[Dependency] = []
@@ -239,5 +334,64 @@ def div(t1: Tensor, t2: Tensor) -> Tensor:
             return grad
 
         dependencies.append(Dependency(t2, grad_fn2))
+
+    return Tensor(data, requires_grad, dependencies)
+
+
+def _matmul(t1: Tensor, t2: Tensor) -> Tensor:
+    """
+    if t1 is (n1, m1) and t2 is (m1, m2), then t1 @ t2 is (n1, m2)
+    so grad3 is (n1, m2)
+    if t3 = t1 @ t2, and grad3 is the gradient of some function wrt t3, then
+        grad1 = grad3 @ t2.T
+        grad2 = t1.T @ grad3
+    """
+    data = t1.data @ t2.data
+    requires_grad = t1.requires_grad or t2.requires_grad
+    dependencies: List[Dependency] = []
+
+    if t1.requires_grad:
+        def grad_fn1(grad: np.ndarray) -> np.ndarray:
+            return grad @ np.swapaxes(t2.data, -1, -2)
+
+        dependencies.append(Dependency(t1, grad_fn1))
+
+    if t2.requires_grad:
+        def grad_fn2(grad: np.ndarray) -> np.ndarray:
+            return np.swapaxes(t1.data, -1, -2) @ grad
+
+        dependencies.append(Dependency(t2, grad_fn2))
+
+    return Tensor(data, requires_grad, dependencies)
+
+
+def _dot(t1: Tensor, t2: Tensor) -> Tensor:
+    pass
+
+
+def _transpose(a: Tensor, dim1: int, dim2: int) -> Tensor:
+    data = np.swapaxes(a.data, dim1, dim2)
+    requires_grad = a.requires_grad
+    dependencies: List[Dependency] = []
+
+    if a.requires_grad:
+        def grad_fn(grad: np.ndarray) -> np.ndarray:
+            return np.swapaxes(grad, dim2, dim1)
+
+        dependencies.append(Dependency(a, grad_fn))
+
+    return Tensor(data, requires_grad, dependencies)
+
+
+def _permute(a: Tensor, axes: Union[List, Tuple]) -> Tensor:
+    data = a.data.transpose(axes)
+    requires_grad = a.requires_grad
+    dependencies: List[Dependency] = []
+
+    if a.requires_grad:
+        def grad_fn(grad: np.ndarray) -> np.ndarray:
+            return grad.transpose(axes)
+
+        dependencies.append(Dependency(a, grad_fn))
 
     return Tensor(data, requires_grad, dependencies)
--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -2,7 +2,10 @@ import os
 import sys
 import unittest
 
-from src.dpln.autograd import Tensor, add, sub, neg, mul, div
+import numpy as np
+import torch
+
+from src.dpln.autograd import Tensor, ones_like
 
 
 class TestTensor(unittest.TestCase):
@@ -24,7 +27,7 @@ class TestTensor(unittest.TestCase):
     def test_add(self):
         self.t1.zero_grad()
         self.t2.zero_grad()
-        sum = add(self.t1, self.t2)
+        sum = self.t1 + self.t2
         self.assertEqual(sum, Tensor([5, 7, 9]))
         sum.backward(Tensor([1, 1, 1]))
         self.assertEqual(self.t1.grad, Tensor([1, 1, 1]))
@@ -33,7 +36,7 @@ class TestTensor(unittest.TestCase):
     def test_sub(self):
         self.t1.zero_grad()
         self.t2.zero_grad()
-        x = sub(self.t1, self.t2)
+        x = self.t1 - self.t2
         self.assertEqual(x, Tensor([-3, -3, -3]))
         x.backward(Tensor([1, 1, 1]))
         self.assertEqual(self.t1.grad, Tensor([1, 1, 1]))
@@ -41,7 +44,7 @@ class TestTensor(unittest.TestCase):
 
     def test_neg(self):
         self.t1.zero_grad()
-        n = neg(self.t1)
+        n = -self.t1
         self.assertEqual(n, Tensor([-1, -2, -3]))
         n.backward(Tensor([1, 1, 1]))
         self.assertEqual(self.t1.grad, Tensor([-1, -1, -1]))
@@ -49,20 +52,141 @@ class TestTensor(unittest.TestCase):
     def test_mul(self):
         self.t1.zero_grad()
         self.t2.zero_grad()
-        x = mul(self.t1, self.t2)
+        x = self.t1 * self.t2
         self.assertEqual(x, Tensor([4, 10, 18]))
         x.backward(Tensor([1, 1, 1]))
         self.assertEqual(self.t1.grad, self.t2)
         self.assertEqual(self.t2.grad, self.t1)
 
-    def test_div(self):
+    def test_truediv(self):
         a = Tensor([1., 2., 1.], requires_grad=True)
         b = Tensor([4.], requires_grad=True)
-        y = div(b, a)
+        y = b / a
         self.assertEqual(y, Tensor([4., 2., 4.]))
         y.backward(Tensor([1., 1., 1.]))
         self.assertEqual(a.grad, Tensor([-4., -1., -4.]))
         self.assertEqual(b.grad, Tensor([2.5]))
+
+    def test_matmul(self):
+        for _ in range(1000):
+            a = np.random.rand(8, 10)
+            b = np.random.rand(10, 8)
+            a0 = Tensor(a, requires_grad=True)
+            b0 = Tensor(b, requires_grad=True)
+            x = a0 @ b0
+            x.backward(ones_like(x))
+            a1 = torch.tensor(a, requires_grad=True)
+            b1 = torch.tensor(b, requires_grad=True)
+            y = a1 @ b1
+            y.backward(torch.ones_like(y))
+            self.assertTrue((x.numpy() == y.detach().numpy()).all())
+            self.assertTrue((a0.grad.numpy() == a1.grad.numpy()).all())
+            self.assertTrue((b0.grad.numpy() == b1.grad.numpy()).all())
+
+        for _ in range(1000):
+            a = np.random.rand(16, 16, 8, 10)
+            b = np.random.rand(16, 16, 10, 8)
+            a0 = Tensor(a, requires_grad=True)
+            b0 = Tensor(b, requires_grad=True)
+            x = a0 @ b0
+            x.backward(ones_like(x))
+            a1 = torch.tensor(a, requires_grad=True)
+            b1 = torch.tensor(b, requires_grad=True)
+            y = a1 @ b1
+            y.backward(torch.ones_like(y))
+            self.assertTrue((x.numpy() == y.detach().numpy()).all())
+            self.assertTrue((a0.grad.numpy() == a1.grad.numpy()).all())
+            self.assertTrue((b0.grad.numpy() == b1.grad.numpy()).all())
+
+    def test_transpose(self):
+        for _ in range(100):
+            a = np.random.rand(3, 4)
+            a0 = Tensor(a, requires_grad=True)
+            b0 = a0.transpose(0, 1)
+            b0.backward(ones_like(b0))
+            a1 = torch.tensor(a, requires_grad=True)
+            b1 = torch.transpose(a1, 0, 1)
+            b1.backward(torch.ones_like(b1))
+            self.assertTrue((b0.numpy() == b1.detach().numpy()).all())
+            self.assertTrue((a0.grad.numpy() == a1.grad.detach().numpy()).all())
+
+        for _ in range(100):
+            a = np.random.rand(3, 4, 5, 6)
+            a0 = Tensor(a, requires_grad=True)
+            b0 = a0.transpose(1, 3)
+            b0.backward(ones_like(b0))
+            a1 = torch.tensor(a, requires_grad=True)
+            b1 = torch.transpose(a1, 1, 3)
+            b1.backward(torch.ones_like(b1))
+            self.assertTrue((b0.numpy() == b1.detach().numpy()).all())
+            self.assertTrue((a0.grad.numpy() == a1.grad.detach().numpy()).all())
+
+        for _ in range(100):
+            a = np.random.rand(7, 9, 2, 4, 8, 3)
+            a0 = Tensor(a, requires_grad=True)
+            b0 = a0.transpose(2, 0)
+            b0.backward(ones_like(b0))
+            a1 = torch.tensor(a, requires_grad=True)
+            b1 = torch.transpose(a1, 2, 0)
+            b1.backward(torch.ones_like(b1))
+            self.assertTrue((b0.numpy() == b1.detach().numpy()).all())
+            self.assertTrue((a0.grad.numpy() == a1.grad.detach().numpy()).all())
+
+    def test_permute(self):
+        # for _ in range(10):
+        #     a = np.random.rand(16)
+        #     a0 = Tensor(a, requires_grad=True)
+        #     b0 = a0.permute([0])
+        #     a1 = torch.tensor(a, requires_grad=True)
+        #     b1 = torch.permute(a1, [0])
+        #     self.assertTrue((b0.numpy() == b1.detach().numpy()).all())
+        #     b0.backward(ones_like(b0))
+        #     b1.backward(torch.ones_like(b1))
+        #     self.assertTrue((a0.grad.numpy() == a1.grad.detach().numpy()).all())
+        #
+        # for _ in range(10):
+        #     a = np.random.rand(3, 4)
+        #     a0 = Tensor(a, requires_grad=True)
+        #     b0 = a0.permute([1, 0])
+        #     a1 = torch.tensor(a, requires_grad=True)
+        #     b1 = torch.permute(a1, [1, 0])
+        #     self.assertTrue((b0.numpy() == b1.detach().numpy()).all())
+        #     b0.backward(ones_like(b0))
+        #     b1.backward(torch.ones_like(b1))
+        #     self.assertTrue((a0.grad.numpy() == a1.grad.detach().numpy()).all())
+        #
+        # for _ in range(10):
+        #     a = np.random.rand(3, 4, 5, 6)
+        #     a0 = Tensor(a, requires_grad=True)
+        #     b0 = a0.permute([0, 1, 3, 2])
+        #     b0.backward(ones_like(b0))
+        #     a1 = torch.tensor(a, requires_grad=True)
+        #     b1 = torch.permute(a1, [0, 1, 3, 2])
+        #     b1.backward(torch.ones_like(b1))
+        #     self.assertTrue((b0.numpy() == b1.detach().numpy()).all())
+        #     self.assertTrue((a0.grad.numpy() == a1.grad.detach().numpy()).all())
+        #
+        # for _ in range(10):
+        #     a = np.random.rand(3, 4, 5, 6, 7, 8, 9, 10)
+        #     a0 = Tensor(a, requires_grad=True)
+        #     b0 = a0.permute([7, 6, 5, 4, 3, 2, 1, 0])
+        #     b0.backward(ones_like(b0))
+        #     a1 = torch.tensor(a, requires_grad=True)
+        #     b1 = torch.permute(a1, [7, 6, 5, 4, 3, 2, 1, 0])
+        #     b1.backward(torch.ones_like(b1))
+        #     self.assertTrue((b0.numpy() == b1.detach().numpy()).all())
+        #     self.assertTrue((a0.grad.numpy() == a1.grad.detach().numpy()).all())
+
+        for _ in range(1):
+            a = np.random.rand(3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+            a0 = Tensor(a.copy(), requires_grad=True)
+            b0 = a0.permute([9, 1, 7, 6, 5, 3, 4, 2, 8, 0])
+            a1 = torch.tensor(a.copy(), requires_grad=True)
+            b1 = torch.permute(a1, [9, 1, 7, 6, 5, 3, 4, 2, 8, 0])
+            self.assertTrue((b0.numpy() == b1.detach().numpy()).all())
+            b1.backward(torch.ones_like(b1))
+            b0.backward(ones_like(b0))
+            self.assertTrue((a0.grad.numpy() == a1.grad.detach().numpy()).all())
 
 
 if __name__ == '__main__':
