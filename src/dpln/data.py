09a53567ce622a2c1b3b919861bfe369a7bf41f8
shizuku <2112165916@qq.com>
2021-10-17 01:27:20 +0800

    refactor(dpln.data): refactor data
    refactor Dataset
    add DataLoader

--- src/dpln/data.py
+++ src/dpln/data.py
@@ -1,47 +1,14 @@
-import numpy as np
+import random
 
-from typing import List, TypeVar
-
-# TODO: refactor data
-
-T = TypeVar('T')
+from typing import List, Callable
 
 
 class Dataset:
+    def __len__(self):
+        raise NotImplementedError
+
     def __getitem__(self, item):
-        pass
-
-    def __len__(self):
-        pass
-
-    def __iter__(self):
-        pass
-
-    def __next__(self):
-        pass
-
-    def enumerate(self, start=0):
-        idx = list(range(start, start + len(self)))
-        return ZipDataset(self, ListDataset(idx))
-
-
-class ListDataset(Dataset):
-    def __init__(self, x: List[T]):
-        self.x = x
-        self.idx = 0
-
-    def batch(self, batch_size: int):
-        r = []
-        for i in range(0, len(self.x), batch_size):
-            tx = self.x[i:i + batch_size]
-            r.append(np.array(tx))
-        return ListDataset(r)
-
-    def __getitem__(self, index: int):
-        return self.x[index]
-
-    def __len__(self):
-        return len(self.x)
+        raise NotImplementedError
 
     def __iter__(self):
         self.idx = 0
@@ -56,18 +23,30 @@ class ListDataset(Dataset):
             return r
 
 
-class ZipDataset(Dataset):
-    def __init__(self, x: Dataset, y: Dataset):
-        self.x = x
-        self.y = y
-        assert len(self.x) == len(self.y)
-        self.idx = 0
-
-    def __getitem__(self, index: int):
-        return self.x[index], self.y[index]
+class DataLoader:
+    def __init__(self, dataset: Dataset, batch_size: int = 1, batch_fn: Callable = None, shuffle: bool = False):
+        if batch_size < 1:
+            raise ValueError("batch_size must >= 1")
+        if batch_size > 1 and batch_fn is None:
+            raise ValueError("batch_fn can't be None when batch_size>1")
+        self.dataset: Dataset = dataset
+        self.batch_size: int = batch_size
+        self.batch_fn = batch_fn
+        self.index_list: List[int] = list(range(len(dataset)))
+        if shuffle:
+            random.shuffle(self.index_list)
 
     def __len__(self):
-        return len(self.x)
+        return len(self.dataset) // self.batch_size
+
+    def __getitem__(self, idx):
+        if self.batch_size == 1:
+            return self.dataset[self.index_list[idx]]
+        else:
+            batch = []
+            for i in range(idx * self.batch_size, (idx + 1) * self.batch_size):
+                batch.append(self.dataset[self.index_list[i]])
+            return self.batch_fn(batch)
 
     def __iter__(self):
         self.idx = 0
