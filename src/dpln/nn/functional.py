2f24f616479c46c1388655cc781194adc4a77300
Azathoth1729 <aa1203528272@gmail.com>
2021-10-16 19:29:54 +0800

    feat(dpln.autograd dpln.nn): relu sigmoid softmax tanh mse_loss cross_entropy mean module.children module.named_children

--- src/dpln/nn/functional.py
+++ src/dpln/nn/functional.py
@@ -18,24 +18,59 @@ def linear(x: Tensor, weight: Tensor, bias: Optional[Tensor] = None) -> Tensor:
     return ret
 
 
-def bilinear(input1: Tensor, input2: Tensor, weight: Tensor, bias: Optional[Tensor] = None) -> Tensor:
-    r"""
-    Applies a bilinear transformation to the incoming data:
-    :math:`y = x_1^T A x_2 + b`
-    """
-    ...
+def relu(x: Tensor) -> Tensor:
+    # TODO: test
+    data = np.maximum(x.data, 0)
+    requires_grad = x.requires_grad
+
+    if requires_grad:
+        def grad_fn(grad: np.ndarray) -> np.ndarray:
+            return grad * (np.where(x.data.any() > 0, 1, 0))
+
+        dependencies = [Dependency(x, grad_fn)]
+    else:
+        dependencies = []
+
+    return Tensor(data, requires_grad, dependencies)
 
 
-def l1_loss(x: Tensor, target: Tensor, reduction: str = 'mean') -> Tensor:
-    pass
+def sigmoid(x: Tensor) -> Tensor:
+    # TODO: test
+    data = 1 / (1 + np.exp(-x))
+    requires_grad = x.requires_grad
+    if requires_grad:
+        def grad_fn(grad: np.ndarray) -> np.ndarray:
+            return grad * data * (1 - data)
+
+        dependencies = [Dependency(x, grad_fn)]
+    else:
+        dependencies = []
+
+    return Tensor(data, requires_grad, dependencies)
 
 
-def mse_loss(x: Tensor, target: Tensor, reduction: str = 'mean') -> Tensor:
-    pass
+def softmax(x: Tensor, axis=-1) -> Tensor:
+    # TODO: test
+    e = np.exp(x.data - np.max(x))
+    data = e / np.sum(e, axis=axis, keepdims=True)  # (bs, n)
+    requires_grad = x.requires_grad
+
+    if requires_grad:
+        def grad_fn(grad: np.ndarray) -> np.ndarray:
+            s = data[:, :, np.newaxis]  # (bs, n, 1)
+            a = np.array([np.diagflat(s[i, :, :]) for i in range(s.shape[0])])  # (bs, n, n)
+            b = np.matmul(s, s.transpose((0, 2, 1)))  # (bs, n, n)
+            return grad * (a - b)
+
+        dependencies = [Dependency(x, grad_fn)]
+    else:
+        dependencies = []
+
+    return Tensor(data, requires_grad, dependencies)
 
 
-# TODO somewhere else
 def tanh(x: Tensor) -> Tensor:
+    # TODO: test
     data = np.tanh(x.data)
     requires_grad = x.requires_grad
 
@@ -43,8 +78,31 @@ def tanh(x: Tensor) -> Tensor:
         def grad_fn(grad: np.ndarray) -> np.ndarray:
             return grad * (1 - data * data)
 
-        depends_on = [Dependency(x, grad_fn)]
+        dependencies = [Dependency(x, grad_fn)]
     else:
-        depends_on = []
+        dependencies = []
 
-    return Tensor(data, requires_grad, depends_on)
+    return Tensor(data, requires_grad, dependencies)
+
+
+def mse_loss(x: Tensor, y: Tensor, reduction: str = 'mean') -> Tensor:
+    # TODO: test
+    L = x - y
+
+    if reduction == 'mean':
+        return L.mean()
+    elif reduction == 'sum':
+        return L.sum()
+    elif reduction is None:
+        return L
+    else:
+        raise ValueError("reduction must be 'mean', 'sum', or None")
+
+
+def cross_entropy(x: Tensor, y: Tensor, weight=None, reduction: str = 'mean') -> Tensor:
+    # TODO: test
+    pass
+
+
+def l1_loss(x: Tensor, target: Tensor, reduction: str = 'mean') -> Tensor:
+    pass
--- src/dpln/nn/modules/activation.py
+++ src/dpln/nn/modules/activation.py
@@ -1,1 +1,34 @@
-# TODO: impl activations
+from dpln import Tensor
+
+from .module import Module
+from .. import init
+from .. import functional as F
+from ..parameter import Parameter
+
+from typing import (
+    Optional,
+)
+
+
+class ReLU(Module):
+
+    def forward(self, x: Tensor) -> Tensor:
+        return F.relu(x)
+
+
+class Sigmoid(Module):
+
+    def forward(self, x: Tensor) -> Tensor:
+        return F.sigmoid(x)
+
+
+class Softmax(Module):
+
+    def forward(self, x: Tensor) -> Tensor:
+        return F.softmax(x)
+
+
+class Tanh(Module):
+
+    def forward(self, x: Tensor) -> Tensor:
+        return F.tanh(x)
