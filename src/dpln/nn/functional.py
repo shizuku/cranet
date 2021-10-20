8448b174444123ecee0b3da759c3f37842042edb
shizuku <2112165916@qq.com>
2021-10-20 16:38:09 +0800

    feat(dpln.nn): impl conv2d wothout padding, dilation, groups

--- src/dpln/nn/functional.py
+++ src/dpln/nn/functional.py
@@ -1,6 +1,8 @@
 from ..autograd.tensor import Tensor
 from ..autograd import function as F
+from .utils import im2col2d
 
+import math
 from typing import (
     Optional,
 )
@@ -16,8 +18,30 @@ def linear(x: Tensor, weight: Tensor, bias: Optional[Tensor] = None) -> Tensor:
     return output
 
 
-def conv2d():
-    pass
+def conv2d(inp: Tensor, weight: Tensor, bias: Tensor, stride=1, padding=0, dilation=1, groups=1):
+    """
+    TODO: impl padding, dilation, groups
+    :param inp: Tensor shape(bs, ch_i, h_i, w_i)
+    :param weight: Tensor shape(ch_o, ch_i, h_k, w_k)
+    :param bias: Tensor shape(ch_o)
+    :param stride: int | Tuple[int, int]
+    :param padding: int | Tuple[int, int]
+    :param dilation: int | Tuple[int, int]
+    :param groups: int
+    :return: Tensor shape(bs, ch_o, h_o, w_o)
+    """
+    if type(stride) == int:
+        stride = (stride, stride)
+    if type(padding) == int:
+        padding = (padding, padding)
+    if type(dilation) == int:
+        dilation = (dilation, dilation)
+    bs, _, h_i, w_i = inp.shape
+    ch_o, ch_i, h_k, w_k = weight.shape
+    h_o = math.floor((h_i + 2 * padding[0] - dilation[0] * (h_k - 1) - 1) / stride[0] + 1)
+    w_o = math.floor((w_i + 2 * padding[1] - dilation[1] * (w_k - 1) - 1) / stride[1] + 1)
+    col = im2col2d(inp, weight.shape, stride)
+    return (weight.reshape(ch_o, ch_i * h_k * w_k) @ col.transpose(1, 2)).reshape(bs, ch_o, h_o, w_o) + bias.reshape(1, ch_o, 1, 1)
 
 
 def relu(x: Tensor) -> Tensor:
--- src/dpln/nn/utils.py
+++ src/dpln/nn/utils.py
@@ -1,5 +1,22 @@
-from ..autograd import Tensor
+from ..autograd import Tensor, concat
+
+from typing import (
+    Union, Tuple, List,
+)
 
 
-def im2col2d(x: Tensor, kernel: Tensor):
-    pass
+def im2col2d(inp: Tensor, kernel_shape: Union[Tuple, List], stride: Tuple):
+    """
+
+    :param inp: Tensor (bs, ch_i, h_i, w_i)
+    :param kernel_shape: (ch_o, ch_i, h_k, w_k)
+    :param stride: Tuple[int, int]
+    :return: Tensor (bs, *, i_ch*k_w*k_h)
+    """
+    bs, _, h_i, w_i = inp.shape
+    _, ch_i, h_k, w_k = kernel_shape
+    ret = []
+    for i in range(0, h_i - h_k + 1, stride[0]):
+        for j in range(0, w_i - w_k + 1, stride[1]):
+            ret.append(inp[:, :, i:i + h_k, j:j + w_k].reshape(bs, 1, ch_i * h_k * w_k))
+    return concat(ret, axis=1)
