69530358ae50f0c817c3e930daf1923e1c90fe0b
shizuku <2112165916@qq.com>
2021-11-03 16:54:03 +0800

    refactor(cranet.autograd): no_grad decorator

--- tests/test_autograd/test_grad_mode.py
+++ tests/test_autograd/test_grad_mode.py
@@ -7,15 +7,37 @@ from src import cranet
 
 class TestNoGrad(unittest.TestCase):
     def test_1(self):
-        print(cranet.is_grad_enabled())
+        b = cranet.tensor(np.random.rand(4, 5, 6), requires_grad=True)
+        self.assertTrue(b.requires_grad is True)
         with cranet.no_grad():
-            print(cranet.is_grad_enabled())
             a = cranet.tensor(np.random.rand(4, 5, 6), requires_grad=True)
             self.assertTrue(a.requires_grad is False)
+        c = cranet.tensor(np.random.rand(4, 5, 6), requires_grad=True)
+        self.assertTrue(c.requires_grad is True)
 
-    def test_0(self):
+    def test_2(self):
         a = cranet.tensor(np.random.rand(4, 5, 6), requires_grad=True)
         b = cranet.tensor(np.random.rand(4, 5, 6), requires_grad=True)
+        e = a * b
+        self.assertTrue(e.requires_grad is True)
         with cranet.no_grad():
             c = a + b
             self.assertTrue(c.requires_grad is False)
+        d = a - b
+        self.assertTrue(d.requires_grad is True)
+
+    def test_3(self):
+        a = cranet.tensor(np.random.rand(4, 5, 6), requires_grad=True)
+        b = cranet.tensor(np.random.rand(4, 5, 6), requires_grad=True)
+        c, d = no_grad_test_3_func(a, b)
+        e = a + b
+        f = a - b
+        self.assertTrue(c.requires_grad is False)
+        self.assertTrue(d.requires_grad is False)
+        self.assertTrue(e.requires_grad is True)
+        self.assertTrue(f.requires_grad is True)
+
+
+@cranet.no_grad()
+def no_grad_test_3_func(a, b):
+    return a + b, a - b
