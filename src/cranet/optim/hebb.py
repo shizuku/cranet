ae6c1324e4f209a246ea74460480036257b1c550
Azathoth1729 <aa1203528272@gmail.com>
2021-11-04 19:34:54 +0800

    remove (cranet.optim): hebb

--- src/cranet/optim/hebb.py
+++ src/cranet/optim/hebb.py
@@ -4,13 +4,3 @@ from .optimizer import Optimizer
 from ..nn.parameter import Parameter
 
 from typing import Iterator
-
-
-class Oja(Optimizer):
-    def __init__(self, parameters: Iterator[Parameter], lr: float = 0.01, *args, **kwargs) -> None:
-        super().__init__(parameters, *args, **kwargs)
-        self.lr = lr
-
-    def step(self, model: Module) -> None:
-        for parameter in self.parameters:
-            parameter = parameter + self.lr
