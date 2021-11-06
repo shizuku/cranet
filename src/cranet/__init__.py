ae8b264ab672f83a25350a61f0b7e9c6f2db645f
shizuku <2112165916@qq.com>
2021-11-06 17:48:05 +0800

    feat(cranet.nn): save module

--- src/cranet/__init__.py
+++ src/cranet/__init__.py
@@ -3,5 +3,6 @@ from . import data
 from . import nn
 from . import optim
 from . import utils
+from .utils.pickle import *
 
 __version__ = "0.1.3"
--- src/cranet/utils/__init__.py
+++ src/cranet/utils/__init__.py
@@ -1,1 +1,3 @@
 from .plot_model import plot_tensor_dep
+
+from .pickle import save, load
